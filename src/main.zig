a31af5f7021c471478c30f3fc304bdfa7e4d8a7e
Andrew Abbott <andrew@mirx.dev>
2021-10-10 23:40:47 +1100

    Start writing init code

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,20 @@
+const assert = @import("std").debug.assert;
+const mem = @import("std").mem;
+
 extern var __data_source_start: u8;
 extern var __data_source_end: u8;
 extern var __data_target_start: u8;
 extern var __data_target_end: u8;
 
-extern var __bss_source_start: u8;
-extern var __bss_source_end: u8;
-extern var __bss_target_start: u8;
-extern var __bss_target_end: u8;
-
-const limit: usize = 1000;
-var i: usize = 6;
+extern var __bss_start: u8;
+extern var __bss_end: u8;
 
 export fn _start() align(4) linksection(".text.start") callconv(.Naked) noreturn {
-    while (i < limit) {
-        i += 1;
-    }
+    const data_length: usize = @ptrToInt(&__data_source_end) - @ptrToInt(&__data_source_start);
+    assert(data_length == (@ptrToInt(&__data_target_end) - @ptrToInt(&__data_target_start)));
+    const data_source: []volatile u8 = (&__data_source_start)[0..data_length];
+    const data_dest: []volatile u8 = (&__data_source_start)[0..data_length];
+    mem.copy(u8, data_dest, data_source);
 
     while (true) {}
 }
