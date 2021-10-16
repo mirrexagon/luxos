6fee5ff21c7606208802d977ba3505c1dee6aaf3
Andrew Abbott <andrew@mirx.dev>
2021-10-17 09:15:45 +1100

    Use master zig

--- shell.nix
+++ shell.nix
@@ -1,8 +1,20 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    zig
+    ((zig.overrideAttrs (oldAttrs: {
+      version = "git";
+
+      src = fetchFromGitHub {
+        owner = "ziglang";
+        repo = oldAttrs.pname;
+        rev = "0536c25578fa15e2326eb1061f6db61d6ad3cd65";
+        hash = "sha256-xxb3jSvmrGHm7KiEo9CiHNxNmT0C+wiyJp9pYwKQBgo=";
+      };
+    })).override {
+      llvmPackages = llvmPackages_13;
+    })
+
     qemu
 
     pkgs.pkgsCross.riscv64.buildPackages.binutils
