9549e7c998b0b8f484b3fc21a26e7bb1838facd2
Andrew Abbott <andrew@mirx.dev>
2023-02-17 14:03:26 +1100

    Use nixpkgs Zig

--- shell.nix
+++ shell.nix
@@ -2,20 +2,7 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    ((zig.overrideAttrs (oldAttrs: {
-      version = "git";
-
-      # Version with fix for packed struct size issues.
-      # https://github.com/ziglang/zig/pull/11279
-      src = fetchFromGitHub {
-        owner = "ziglang";
-        repo = oldAttrs.pname;
-        rev = "cf20b97b713d992d84fdd8935ef935f61ed6d747";
-        hash = "sha256-j9/+TkjAy6H4saltz6l5s0BrHfmP56LDocOD4tP2RmI=";
-      };
-    })).override {
-      llvmPackages = llvmPackages_13;
-    })
+    zig
 
     qemu
 
