cdf86c8ba65f731b5cf0cb2edb292ade766902a2
Andrew Abbott <andrew@mirx.dev>
2023-02-22 21:10:13 +1100

    TEMPORARY: Don't log from libc assert
    With Zig 0.10.1, the log statement now triggers a check for freestanding
    targets having root.log defined in the compilation unit. Since we are
    compiling the libc files separately, they don't have their own root.log.
    
    How can I make the libc files compile as part of the main kernel
    compilation unit?

--- src/libc/assert.zig
+++ src/libc/assert.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 
 export fn _libc_assert(condition_string: [*:0]const u8, condition: c_int) void {
     if (condition == 0) {
-        std.log.err("Assertion failed: {}", .{condition_string});
+        //std.log.err("Assertion failed: {}", .{condition_string});
+        _ = condition_string;
         @panic("assert() from libc was called");
     }
 }
